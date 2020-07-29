b3e35ed8796228ed35e40fda11c97b19a87e90d1
Akash Barsagadey <akash.bnb2011@gmail.com>
2020-07-29 10:41:53 +0530

    added loadrs

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,19 +1,67 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
-}
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        exclude: [/cropwise-light\.less$/],
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              modules: true,
+              javascriptEnabled: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        include: [/cropwise-light\.less$/],
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              modules: false,
+              javascriptEnabled: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
