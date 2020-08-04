6fadd8994610b9b1a59a77b61632663fcabe463d
Akash Barsagadey <akash.bnb2011@gmail.com>
2020-08-04 14:43:48 +0530

    try function component

--- src/hello-world.jsx
+++ src/hello-world.jsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 
 
 
-
+/* 
 export default class HelloWorld extends React.Component {
     render() {
         return <div>Hello world using Typescript & React!</div>;
     }
-}
+} */
+
+export const HelloWorld = (props) => {
+    return <div>Hello world using Typescript & Akas4</div>;
+  };
